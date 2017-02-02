52b37df9afc7789888bc40e3f2ab67a49485d24a
Akagi <kaya3728@gmail.com>
2017-02-02 19:26:56 +0900

    完成版

--- assosiation_estimation2.py
+++ assosiation_estimation2.py
@@ -9,9 +9,9 @@ from multiprocessing import Process
 import gc
 
 print('now, data importintg....')
-data = pd.read_csv('../test.csv')
-#data = pd.read_csv('../2016-10-12TRD2011TSCA_weight_fitted.csv')
-#data = data.loc[0:1000,['new_HNMKM_KJ','hc_j5_cd','jc_j5_cd','weight_fitted','HC_KG_CD','JC_KG_CD','hc_KG_SGY_CD','jc_KG_SGY_CD']]
+#data = pd.read_csv('../test.csv') #テスト用
+data = pd.read_csv('../2016-10-12TRD2011TSCA_weight_fitted.csv')
+data = data.loc[0:1000,['new_HNMKM_KJ','hc_j5_cd','jc_j5_cd','weight_fitted','HC_KG_CD','JC_KG_CD','hc_KG_SGY_CD','jc_KG_SGY_CD']]
 #欠損値の除去
 data = data.dropna()
 #indexの振り直し
@@ -74,6 +74,7 @@ output_data.to_csv('../renkan_temp.csv')
 #--業種の組み合わせ毎の品目の組み合わせ毎の取引額の平均割合を作成する
 #----dataをソート
 output_data = output_data.sort_values(by = ["Ind_pair","Input_industory","Output_industry"],ascending=True)
+output_data = output_data.reset_index(drop=True)
 
 temp_ind_pair = output_data['Ind_pair'][0]
 temp_input_ind = output_data['Input_industory'][0]
@@ -81,13 +82,13 @@ temp_output_ind = output_data['Output_industry'][0]
 temp_value = output_data['Value'][0]
 ind_pair_goods_pair_value_list = [[temp_ind_pair,temp_input_ind,temp_output_ind,temp_value,1]] #[ind_pair,品目,品目,額] 
 out_line_num = 0 #ind_pair_goods_pair_value_list の桁数
-same_input_count =0
+same_input_count = 1
 same_input_total = 0
 
 
 """
 業種組み合わせ,投入商品,産出商品,の順で異なった場合に行が変わる
-業種ー投入商品が同じ商品の総額,組み合わせ数を記録し異なった場合に総額から割合を出す
+業種が同じ総額,産出商品の組み合わせ数を記録し業種が異なった場合に総額から割合を出す
 業種ー投入商品ー産出商品が同じ限り,組み合わせ数を記録し,異なった場合に平均を出す
 """
 
@@ -108,15 +109,16 @@ for num in  range(len(output_data['Ind_pair'])):
         ind_pair_goods_pair_value_list[out_line_num][3] /= ind_pair_goods_pair_value_list[out_line_num][4] 
 
         
-        same_input_total += ind_pair_goods_pair_value_list[out_line_num][3]
-        same_input_count += 1
 
-        for i in range(same_input_count):
-            ind_pair_goods_pair_value_list[-i][3] /= same_input_total
+        for i  in range(same_input_count):
+            if same_input_total == 0:
+                ind_pair_goods_pair_value_list[out_line_num - i][3] = 1
+            else:
+                ind_pair_goods_pair_value_list[out_line_num - i][3] /= same_input_total
         
         out_line_num += 1 
-        same_input_count = 0
-        same_input_total = 0
+        same_input_count = 1
+        same_input_total = value
 
     else:
         if input_ind != temp_input_ind:
@@ -129,15 +131,8 @@ for num in  range(len(output_data['Ind_pair'])):
             ind_pair_goods_pair_value_list[out_line_num][3] /= ind_pair_goods_pair_value_list[out_line_num][4] 
             out_line_num += 1
 
-            same_input_total += ind_pair_goods_pair_value_list[out_line_num][3]
+            same_input_total += value
             same_input_count += 1
-
-            for i in range(same_input_count):
-                ind_pair_goods_pair_value_list[-i][3] /= same_input_total
-
-            
-            same_input_count = 0
-            same_input_total = 0
             
 
         elif input_ind == temp_input_ind:
@@ -151,21 +146,26 @@ for num in  range(len(output_data['Ind_pair'])):
                 ind_pair_goods_pair_value_list.append(out_line)
                 ind_pair_goods_pair_value_list[out_line_num][3] /= ind_pair_goods_pair_value_list[out_line_num][4] 
 
-                same_input_total += ind_pair_goods_pair_value_list[out_line_num][3]
+                same_input_total += value
                 same_input_count +=1
                 out_line_num += 1
 
             elif output_ind == temp_output_ind:
                 ind_pair_goods_pair_value_list[out_line_num][3] += value
                 ind_pair_goods_pair_value_list[out_line_num][4] += 1
-                
+                same_input_total += value 
 
-                #最後のデータの場合にvalueを割っておく
-                if num == len(list(output_data['Ind_pair'])):
-                    ind_pair_goods_pair_value_list[out_line_num][3] /= ind_pair_goods_pair_value_list[out_line_num][4] 
-                     
-                    for i in range(same_input_count):
-                        ind_pair_goods_pair_value_list[-i][3] /= same_input_total
+        #最後のデータの場合にvalueを割っておく
+        if num == len(list(output_data['Ind_pair'])) - 1:
+            ind_pair_goods_pair_value_list[out_line_num][3] /= ind_pair_goods_pair_value_list[out_line_num][4] 
+
+            same_input_count += 1
+            for i in range(same_input_count):
+                if same_input_total == 0:
+                    ind_pair_goods_pair_value_list[out_line_num - i][3] = 1
+                else:
+                    ind_pair_goods_pair_value_list[out_line_num - i][3] /= same_input_total
+                        
 
              
     temp_ind_pair = ind_pair
@@ -189,9 +189,9 @@ for i in range(len(only_h_trans)):
     for j in range(len(ind_pair_goods_pair_value_list)):
         out_line = []
         if temp_ind_pair == ind_pair_goods_pair_value_list[j][0]:
-            temp_value *= ind_pair_goods_pair_value_list[j][3] 
+            value = temp_value * ind_pair_goods_pair_value_list[j][3] 
             temp_output_ind = ind_pair_goods_pair_value_list[j][2]
-            out_line = [temp_input_area,temp_output_area,temp_input_ind,temp_output_ind,temp_value,temp_ind_pair]
+            out_line = [temp_input_area,temp_output_area,temp_input_ind,temp_output_ind,value,temp_ind_pair]
             only_h_trans_value.append(out_line)
 
 del only_h_trans, ind_pair_goods_pair_value_list
